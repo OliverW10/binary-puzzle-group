5c06c31d879c2419d6997216cf5ab93768e3ff05
oliverw10 <oliver@warrickfamily.com>
2021-04-21 12:57:44 +1000

    added threes check in checkBoard

--- v1.05/script.js
+++ v1.05/script.js
@@ -61,6 +61,21 @@ function getGrid(){
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+function arraysEqual(a, b) {
+	if (a === b) return true;
+	if (a == null || b == null) return false;
+	if (a.length !== b.length) return false;
+
+	// If you don't care about the order of the elements inside
+	// the array, you should sort both arrays here.
+	// Please note that calling sort on an array will modify that array.
+	// you might want to clone your array first.
+
+	for (var i = 0; i < a.length; ++i) {
+		if (a[i] !== b[i]) return false;
+	}
+	return true;
+}
 
 function checkBoard(board){
 	maxNum = 3; // maximum number of each number to be in each row/col
@@ -68,14 +83,16 @@ function checkBoard(board){
 
 	rowCounts = [0, 0]
 	colCounts = [0, 0]
+
 	// Counts how many 1's and 0's are in each row and coloumn of the board
+	// Fails if there are more than maxNum of either in any row or col
 	for(var i = 0; i < 6; i += 1){
 		for(var j = 0; j < 6; j+= 1){
 
-			if(board[j][i]] != -1){
+			if(board[j][i] != -1){
 				rowCounts[ board[j][i] ] += 1
 			}
-			if(board[i][j]] != -1){
+			if(board[i][j] != -1){
 				colCounts[ board[i][j] ] += 1
 			}
 		}
@@ -85,7 +102,35 @@ function checkBoard(board){
 		rowCounts = [0, 0]
 		colCounts = [0, 0]
 	}
-	//
+
+	isThree = (arr) => arraysEqual(arr, [0, 0, 0]) || arraysEqual(arr, [1, 1, 1]);
+	// Checks if there is three of the same number in a line
+	for(var i = 1; i < 5; i += 1){
+		for(var j = 0; j < 6; j += 1){
+
+			// gets the values for the grid squares on either side up and down of this one
+			horiz = [ board[i-1][j], board[i][j], board[i+1][j] ];
+			vert =  [ board[j][i-1], board[j][i], board[j][i+1] ];
+
+			if(isThree(horiz) || isThree(vert)){
+				return false
+			}
+
+		}
+	}
+
+	// Only check for uniqueness if the board is full
+	for(var i = 0; i < 6; i += 1){
+		for (var j = 0; j < 6; j += 1){
+			if(board[i][j] == -1){
+				return true
+			}
+		}
+	}
+	// Make sure every row and colomn is unique
+
+
+	return true
 }
 
 function branch(){
@@ -98,6 +143,7 @@ function solve(){
 	*/
 	startGrid = getGrid();
 	console.log(startGrid)
+	console.log(checkBoard(startGrid))
 }
 
 createGrid()
